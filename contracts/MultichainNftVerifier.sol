8846375516dea5082cf7b95153afa35836298e50
marcoruggeri <4high20@gmail.com>
2023-05-13 17:34:30 +0100

    remove polygon id

--- contracts/MultichainNftVerifier.sol
+++ contracts/MultichainNftVerifier.sol
@@ -2,13 +2,13 @@
 pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./ZKPVerifier.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IWorldID.sol";
 import "./interfaces/ILensHub.sol";
 import "./interfaces/IBridge.sol";
 import "./helpers/ByteHasher.sol";
 
-contract MultichainNftVerifier is ERC721, ZKPVerifier {
+contract MultichainNftVerifier is ERC721, Ownable {
     using ByteHasher for bytes;
 
     uint64 public constant TRANSFER_REQUEST_ID = 1;
@@ -17,9 +17,6 @@ contract MultichainNftVerifier is ERC721, ZKPVerifier {
     mapping(address => bool) public registeredAddress;
     mapping(uint256 => bool) public registeredLens;
     mapping(uint256 => bool) public registeredWorldcoin;
-
-    mapping(uint256 => address) public idToAddress;
-    mapping(address => uint256) public addressToId;
 
     mapping(uint256 => bytes) public registeredToData;
 
@@ -66,23 +63,6 @@ contract MultichainNftVerifier is ERC721, ZKPVerifier {
         registeredToData[totalRegistered] = _data;
     }
 
-    function registerWithPolygonId(
-        uint64 requestId,
-        uint256[] memory inputs,
-        ICircuitValidator validator,
-        uint16 _chainId,
-        bytes calldata _data
-    ) external {
-        require(!registeredAddress[_msgSender()], "already registered");
-        require(totalRegistered < MAX_REGISTERED, "max registered");
-        require(bridge.multiPassCheck(_msgSender(), _chainId));
-        registeredAddress[_msgSender()] = true;
-        _beforeProofSubmit(requestId, inputs, validator);
-        _afterProofSubmit(requestId, inputs, validator);
-        totalRegistered++;
-        registeredToData[totalRegistered] = _data;
-    }
-
     function registerWithWorldcoin(
         address signal,
         uint256 root,
@@ -109,46 +89,13 @@ contract MultichainNftVerifier is ERC721, ZKPVerifier {
         registeredToData[totalRegistered] = _data;
     }
 
-    function registerWithoutId(uint16 _chainId) external {
+    function registerWithoutId(uint16 _chainId, bytes calldata _data) external {
         require(!registeredAddress[_msgSender()], "already registered");
         require(bridge.multiPassCheck(_msgSender(), _chainId));
         registeredAddress[_msgSender()] = true;
     }
 
     function finalizeRegister() external {}
-
-    function _beforeProofSubmit(
-        uint64 /* requestId */,
-        uint256[] memory inputs,
-        ICircuitValidator validator
-    ) internal view override {
-        // check that the challenge input of the proof is equal to the _msgSender()
-        address addr = GenesisUtils.int256ToAddress(
-            inputs[validator.getChallengeInputIndex()]
-        );
-        require(
-            _msgSender() == addr,
-            "address in the proof is not a sender address"
-        );
-    }
-
-    function _afterProofSubmit(
-        uint64 requestId,
-        uint256[] memory inputs,
-        ICircuitValidator validator
-    ) internal override {
-        require(
-            requestId == TRANSFER_REQUEST_ID && addressToId[_msgSender()] == 0,
-            "proof can not be submitted more than once"
-        );
-
-        uint256 id = inputs[validator.getChallengeInputIndex()];
-
-        if (idToAddress[id] == address(0)) {
-            addressToId[_msgSender()] = id;
-            idToAddress[id] = _msgSender();
-        }
-    }
 
     function _baseURI() internal view virtual override returns (string memory) {
         return uri;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,50 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const MultichainTicket = await ethers.getContractFactory("MultichainTicket");
-  const multichainTicket = await MultichainTicket.deploy(""); // add uri
+  // const MultichainTicket = await ethers.getContractFactory("MultichainTicket");
+  // const multichainTicket = await MultichainTicket.deploy(""); // add uri
 
-  await multichainTicket.deployed();
+  // await multichainTicket.deployed();
 
-  console.log("multichainTicket deployed to:", multichainTicket.address);
+  // console.log("multichainTicket deployed to:", multichainTicket.address);
+
+  const PoseidonUnit6L = await ethers.getContractFactory("PoseidonUnit6L");
+  const poseidonUnit6L = await PoseidonUnit6L.deploy();
+  await poseidonUnit6L.deployed();
+
+  const SpongePoseidon = await ethers.getContractFactory("SpongePoseidon", {
+    libraries: {
+      "contracts/lib/Poseidon.sol:PoseidonUnit6L": poseidonUnit6L.address,
+    },
+  });
+  const spongePoseidon = await SpongePoseidon.deploy();
+  await spongePoseidon.deployed();
+
+  let MultichainNftVerifier = await ethers.getContractFactory(
+    "MultichainNftVerifier",
+    {
+      libraries: {
+        "contracts/lib/Poseidon.sol:PoseidonUnit6L": poseidonUnit6L.address,
+        "contracts/lib/Poseidon.sol:SpongePoseidon": spongePoseidon.address,
+      },
+    }
+  );
+
+  const multichainNftVerifier = await MultichainNftVerifier.deploy(
+    ethers.constants.AddressZero,
+    ethers.constants.AddressZero,
+    ethers.constants.AddressZero,
+    "",
+    "",
+    ""
+  ); // add uri
+
+  await multichainNftVerifier.deployed();
+
+  console.log(
+    "multichainNftVerifier deployed to:",
+    multichainNftVerifier.address
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
